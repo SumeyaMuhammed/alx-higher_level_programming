503a05c40d84136e26d17a5c083b00c49acfd117
Sumeya Muhammed <sumenaweya@gmail.com>
2024-08-13 03:20:41 -1100

    Displays the last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,7 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 lastDigit = number % 10
-if number < 0:
+if number < 0 & lastDigit != 0:
     lastDigit = 10 - lastDigit
 if lastDigit > 5:
     desc = "and is greater than 5"
