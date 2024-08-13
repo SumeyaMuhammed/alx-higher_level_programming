d43712d2ecba847b04756fae0c470b2a35ed0cc5
Sumeya Muhammed <sumenaweya@gmail.com>
2024-08-13 02:53:27 -1100

    Displays the last digit

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,7 +5,7 @@ lastDigit = number % 10
 if lastDigit > 5:
     desc = "and is greater than 5"
 elif lastDigit == 0:
-    desc ="and is 0"
+    desc = "and is 0"
 else:
     desc = "and is less than 6 and not 0"
 print(f"Last digit of {number} is {lastDigit} {desc}")
