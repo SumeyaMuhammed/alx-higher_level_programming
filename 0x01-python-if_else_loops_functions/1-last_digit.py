ff8156c94b3aef4d949ce919d7d2a3203db55d3d
root <root@DESKTOP-2HAMP0O.localdomain>
2024-08-13 03:13:30 -1100

    Displays the last number

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,10 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 lastDigit = number % 10
+if number < 0:
+    lastDigit = 10 - lastDigit
 if lastDigit > 5:
     desc = "and is greater than 5"
 elif lastDigit == 0:
     desc = "and is 0"
-else:
+elif 0 < lastDigit < 6:
     desc = "and is less than 6 and not 0"
 print(f"Last digit of {number} is {lastDigit} {desc}")
